4f6506310b30a7a063075159dc5c912ad11f5b7e
federavad <fedvadim-y@yandex.ru>
2020-03-17 13:54:18 +0200

    Add files via upload

---
+++ osc_FE_energy.m
@@ -0,0 +1,21 @@
+omega = 2;
+P = 2*pi/omega;
+dt = P/20;
+T = 3*P;
+N_t = floor(round(T/dt)); % round toward negative infinity
+t = linspace(0, N_t*dt, N_t+1); % generate linearly spaced vector
+u = zeros(N_t+1, 1); % create array with zeros
+v = zeros(N_t+1, 1);
+X_0 = 2; % initial conditions
+u(1) = X_0;
+v(1) = 0;
+
+for n = 1:N_t
+    u(n+1) = u(n) + dt*v(n);
+    v(n+1) = v(n) - dt*omega^2*u(n);
+end
+
+[U, K] = osc_energy(u, v, omega);
+plot(t, U+K, 'b-');
+xlabel('t');
+ylabel('U+K');
